c79d835aa77f193f5ca6e416d5a006052d8fd2a0
cmlonder <cemalonder1@gmail.com>
2016-10-09 23:24:32 +0300

    Finished Breath First Search

---
+++ assets/js/bfs.js
@@ -0,0 +1,106 @@
+function create2DArray(rows) {
+  var arr = [];
+
+  for (var i=0;i<rows;i++) {
+     arr[i] = [];
+  }
+
+  return arr;
+}
+
+function createAdjList(nodes, links) {
+  console.log(nodes);
+  var adjList = create2DArray(nodes.length);
+
+  for (var i = 0; i < links.length; i++) {
+    adjList[links[i].source.group].push(links[i].target.group);
+    adjList[links[i].target.group].push(links[i].source.group);
+  }
+
+  // remove duplicate adjantency
+  for (var i = 0; i < nodes.length; i++) {
+    adjList[i] = adjList[i].filter((v, i, a) => a.indexOf(v) === i); 
+  }
+
+  console.log(adjList);
+
+  return adjList;
+}
+
+
+var Queue = function() {
+ this.items = [];
+};
+
+Queue.prototype.enqueue = function(obj) {
+ this.items.push(obj);
+};
+
+Queue.prototype.dequeue = function() {
+ return this.items.shift();
+};
+
+Queue.prototype.isEmpty = function() {
+ return this.items.length === 0;
+};
+
+var doBFS = function(graph, source, target) {
+  var bfsInfo = [];
+  for (var i = 0; i < graph.length; i++) {
+     bfsInfo[i] = {
+      // distance not necessary for unweighted graphs, it can be computed while backtracing
+       distance: null,
+       predecessor: null }
+     ;
+  }
+
+  bfsInfo[source].distance = 0;
+  var queue = new Queue();
+  queue.enqueue(source);
+
+while (!queue.isEmpty()) {
+  var deque= queue.dequeue();
+  var element = document.getElementById(deque);
+  element.className = "";
+  element.classList.add("visited");
+
+  if (deque == target)
+    break;
+  for (i = 0; i < graph[deque].length; i++){
+     var neighbor = graph[deque][i];
+     // distance is used for checking if it is visited or not
+     if(bfsInfo[neighbor].distance === null){
+        bfsInfo[neighbor].distance = bfsInfo[deque].distance + 1;
+        bfsInfo[neighbor].predecessor = deque;
+        queue.enqueue(neighbor);
+     }
+   }
+}
+
+ return bfsInfo;
+};
+
+function backtrace(graph, source, target) {
+  var element = document.getElementById(source);
+
+  element.className = "";
+  element.classList.add("selected");
+  console.log("target: " + target);
+  while(target != source) {
+    element = document.getElementById(target);
+    target = graph[target].predecessor;
+    console.log("path: " + target);  
+
+    element.className = "";
+    element.classList.add("selected");
+  }
+}
+
+function getPath(from, to) {
+  var adjList = createAdjList(nodes, links);
+  var bfsInfo = doBFS(adjList, from, to);
+  // for (var i = 0; i < adjList.length; i++) {
+  //     console.log("vertex " + i + ": distance = " + bfsInfo[i].distance + ", predecessor = " + bfsInfo[i].predecessor);
+  // }
+  backtrace(bfsInfo, from, to);
+}
--- assets/js/input.js
+++ assets/js/input.js
@@ -11,6 +11,9 @@ readFile = function () {
 
   reader.onload = function () {
     document.getElementsByClassName("inner cover")[0].style.display = "none";
+    document.getElementsByClassName("dropdown")[0].style.display    = "initial";
+    document.getElementsByClassName("dropdown")[1].style.display    = "initial";
+
     
     links          = csvJSON(reader.result);
     nodes          = getNodes(links);
@@ -18,7 +21,8 @@ readFile = function () {
     graph["links"] = links;
     graph["nodes"] = nodes;
 
-    cemal();
+    initializeGraph();
+    initializeDrowdowns(nodes);
   };
 
   // start reading the file. When it is done, calls the onload event defined above.
@@ -33,7 +37,7 @@ fileInput.addEventListener('change', readFile);
 function csvJSON(csv){
 
   var lines   = csv.split("\n");
-  var headers = ["from", "to", "cost"];
+  var headers = ["source", "target", "cost"];
   var json    = [];
 
   for(var i = 1; i < lines.length ; i++) {
@@ -50,63 +54,145 @@ function csvJSON(csv){
   return json; 
 }
 
-function getNodes(json) {
+function getNodes(list) {
 
   var lookup  = {};
-  var items   = json;
   var result  = [];
 
-  for (var i = 0; i < items.length; i++) {
+  for (var i = 0; i < list.length; i++) {
     var obj   = {};
-    var item  = items[i];
-    var from  = item.from;
+    var node  = list[i];
+    var source  = node.source;
 
-    if( !(from in lookup)) {
-      lookup[from] =  1;
-      obj["id"]      =  from;
+    if( !(source in lookup)) {
+      lookup[source]   =  1;
+      obj["group"]   =  result.length ;
+      obj["id"]      =  source;
       result.push(obj);
     }
+
+   
   }
 
   return result;
 }
 
-function cemal() {
+function initializeGraph() {
 
-  var width         = 600,
-      height        = 600;
+  var width         = 800,
+      height        = 800;
+
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
 
   var simulation    = d3.forceSimulation()
                         .force("link", d3.forceLink().id(function(d) { return d.id; }))
-                        .force("charge", d3.forceManyBody())
+                        .force("charge", d3.forceManyBody().theta(50))
+                        .force("collusion", d3.forceCollide(70))
                         .force("center", d3.forceCenter(width / 2, height / 2));
 
-  var svgContainer  = d3.select(".playground")
+  var svg  = d3.select(".graph")
                         .append("svg")
                         .attr("width", width)
                         .attr("height", height);
 
-  var node          = svgContainer.append("g")
-                                  .attr("class", "nodes")
-                                  .selectAll("circle")
-                                  .data(graph.nodes)
-                                  .enter()
-                                  .append("circle")
-                                  .attr("r", 10);
+  var link          = svg.selectAll("line")
+                            .data(graph.links)
+                            .enter()
+                            .append("line")
+                            .attr("class", "link")
+                            .attr("from", function (d) { return d.source })
+                            .attr("to", function(d) { return d.target })
+                            .attr("stroke", function (d, i) { return color(i); });
+
+  var node          = svg.selectAll("circle")
+                            .data(graph.nodes)
+                            .enter()
+                            .append("circle")
+                            .attr("class", "node")
+                            .attr("r", 15)
+                            .attr("id", function (d) { return d.group; })
+                            .attr("fill", function (d,i) { return color(d.group); });
+
+  var label         = svg.selectAll("text")
+                          .data(graph.nodes)
+                          .enter()
+                          .append("text")
+                          .attr("class", "name")
+                          .text(function(d) { return d.id; });
+
+
   simulation
       .nodes(graph.nodes)
       .on("tick", ticked);
 
+  simulation.force("link")
+      .links(graph.links)
+      .distance(function(d) { return d.cost });
 
   function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
     node
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
+
+    label
+        .attr("x", function(d)  { return d.x; })
+        .attr("y", function (d) { return d.y - 25; });
+
   }
 
 }
 
-// function ticked() {
-//     .attr("cx", function(d) { return d.x; })
-//     .attr("cy", function(d) { return d.y; });
-// }
+function initializeDrowdowns(nodes) {
+  var source = false;
+  var target = false;
+  var from;
+  var to;
+
+  for(var i = 0; i < nodes.length; i++) {
+    var li       = document.createElement("li");
+    var a        = document.createElement("a");
+    a.setAttribute("data-value", nodes[i].group);
+    var text     = document.createTextNode(nodes[i].id);
+    li.appendChild(a).appendChild(text);
+    var li_clone = li.cloneNode(true);
+
+    document.getElementById("sourceList").appendChild(li);
+    document.getElementById("targetList").appendChild(li_clone);
+  }
+
+  $("#sourceList li a").click(function(){
+    $(".graph svg .node").removeClass("visited");
+    $(".graph svg .node").removeClass("selected");
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    source = true;
+    begin(source, target);
+  });
+
+  $("#targetList li a").click(function(){
+    $(".graph svg .node").removeClass("visited");
+    $(".graph svg .node").removeClass("selected");
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    target = true;
+    begin(source, target);
+  });
+
+}
+
+function begin(source, target) {
+  if(source && target) {
+    from = $("#source").val();
+    to   = $("#target").val();
+    console.log("from" + from + " to "+ to);
+    getPath(from, to);
+  }
+}
